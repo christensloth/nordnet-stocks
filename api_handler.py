9176f0837b579d60d37ecf1200f79e279af46265
Kasper Sloth <kasper.sloth99@gmail.com>
2025-05-12 18:05:39 +0200

    Works, fetches tickers from yahoo finance

--- api_handler.py
+++ api_handler.py
@@ -1,31 +1,31 @@
 import requests
 from datatypes import DataType
+from data_parser import DataParser
 
 class NewtonApi:
     _base_api = "https://api.newtonanalytics.com/price/?"
     _session = requests.Session()
     _session.headers['Origin'] = _base_api
-    def __combine_enums(self, enums: DataType):
-        return ''.join(str(enum.int_value) for enum in enums)
-
+    
     def __init__(self):
         return
+
+    def __combine_enums(self, enums: DataType):
+        return ''.join(str(enum.int_value) for enum in enums)
     
-    def __parse_list(self):
-        with open('tickers.txt', 'r') as file:
-            return [line.strip() for line in file if line.strip()]
-
     def __parse_request(self, datatypes: DataType, ticker: str, interval: str, observations):
-        datatypes = "&dataType="+self.__combine_enums(datatypes)
-        ticker = "&ticker="+ticker
-        interval = "&interval="+interval
-        observations = "&observations="+str(observations)
-        return self._base_api+datatypes+ticker+interval+observations
+        datatypes = "&dataType=" + self.__combine_enums(datatypes)
+        ticker = "&ticker=" + ticker
+        interval = "&interval=" + interval
+        observations = "&observations=" + str(observations)
+        return self._base_api + datatypes + ticker + interval + observations
 
-    """
-    Request for newton API
-    interval is used like: 1mo, 2d, 3w etc..
-    """
-    def make_request(self, datatypes: DataType, ticker: str, interval: str, observations):
+    def make_request(self, datatypes: DataType, ticker: str, interval: str, observations, destination: str):
         request = self.__parse_request(datatypes, ticker, interval, observations)
-        return self._session.get(request).json()
+        parser = DataParser(self.__combine_enums(datatypes))
+        print("Request: " + request)
+        data = self._session.get(request).json()
+        if data['status'] != '200':
+            exit(f"Errorcode was: {data['status']}, reason: {data['data']}")
+        print("API Request for " + ticker + " was successful")
+        parser.parse_data(self._session.get(request).json(), ticker, destination)
---
+++ ticker_parser.py
@@ -0,0 +1,22 @@
+from datatypes import DataType
+from api_handler import NewtonApi
+import os
+
+class TickerParser:
+    _api = NewtonApi()
+    _outputdir = "output/"
+
+    def __init__(self, datatypes: DataType, interval: str, observations):
+        self._datatypes = datatypes
+        self._interval = interval
+        self._observations = observations
+
+    def __parse_list(self):
+        with open('tickers.txt', 'r') as file:
+            return [line.strip() for line in file if line.strip()]
+
+    def fetch_tickers(self):
+        tickers = self.__parse_list()
+        os.makedirs(self._outputdir, exist_ok=True)
+        for ticker in tickers:
+            self._api.make_request(self._datatypes, str(ticker), self._interval, self._observations, self._outputdir)

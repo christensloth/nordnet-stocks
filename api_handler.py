7e3ffe0100da5ceeb364ff4bd72228cecd962147
Kasper Sloth <kasper.sloth99@gmail.com>
2025-05-22 19:30:59 +0200

    cleanup and fix

--- api_handler.py
+++ api_handler.py
@@ -50,7 +50,7 @@ class NewtonApi:
         print("Failed to establish connection after multiple attempts")
         return None
 
-    def call(self, datatypes: DataType, ticker: str, interval: str, observations, destination: str):
+    def call(self, datatypes: DataType, ticker: str, interval: str, observations):
         parser = DataParser(self.__combine_enums(datatypes))
         data = self.__make_request(datatypes, ticker, interval, observations)
-        parser.parse_data(data, ticker, destination)
+        parser.parse_data(data, ticker)

10958494b717035fe51efee0f0a99688ace1ab00
christensloth <christen.sloth@gmail.com>
2025-07-26 17:03:18 +0200

    Add ticker list input argument

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from ticker_parser import TickerParser
 from  datatypes  import DataType
+import argparse
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-t", "--tickers", required = True, help = "Absolute path to .txt file with tickers")
+
+args = parser.parse_args()
 """
 Interval should be in format 1d (1 day), 1wk (1 week), 1m (1 month)
 """
@@ -13,4 +19,4 @@ Available datatypes an be seen in datatypes.py
 datatypes = [DataType.Date, DataType.OpenPrice, DataType.HighPrice, DataType.LowPrice, DataType.ClosePrice, DataType.Volume]
 
 parser = TickerParser(datatypes, data_interval, observations)
-parser.fetch_tickers()
+parser.fetch_tickers(args.tickers)
